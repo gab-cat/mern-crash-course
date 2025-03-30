f444ecb77ebe19cbcc1f07c6c51b333175822fe2
mechy88 <miguelandrerepatacodo@gmail.com>
2025-03-30 19:50:19 +0800

    Minor Mods to Backend

--- backend/server.js
+++ backend/server.js
@@ -7,11 +7,13 @@ dotenv.config();
 
 const app = express();
 
+const PORT = process.env.PORT || 5000;
+
 app.use(express.json()); // allows us to accept json data in the body
 
 app.use("/api/products", productRoutes);
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
     connectDB();
-    console.log("Server started at http://localhost:5000");
+    console.log("Server started at http://localhost: " + PORT);
 });
